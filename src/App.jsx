63b360b093d64b132e5e957724855fc230795ce9
Garrett Ring <garrett.ring57@gmail.com>
2023-07-24 15:26:47 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import RateChickens from "./pages/RateChickens";
+import SubmitChicken from "./pages/SubmitChicken";
+// import ViewChickens from "./pages/ViewChickens";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +11,11 @@ function App() {
   return (
     <>
      <Header />
+     <Routes>
+      <Route path="/" element={<RateChickens />} />
+      <Route path="/submit" element={<SubmitChicken />} />
+      <Route path="/view" element={<ViewChickens />} />
+    </Routes>
     </>
   )
 }
